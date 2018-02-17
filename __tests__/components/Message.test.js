247c8a8bb2becc924f4dd5c3d82444840df87f36
Andros <wong22@hotmail.co.uk>
2018-02-16 22:38:46 -0800

    Implement chai enzyme tests for style checks

--- __tests__/components/Message.test.js
+++ __tests__/components/Message.test.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import expect from 'expect';
-import { shallow } from 'enzyme';
-
+import { shallow, render, mount } from 'enzyme';
+import chai from 'chai';
+import chaiEnzyme from 'chai-enzyme';
 import Message from '../../frontend/components/Message';
 
+chai.use(chaiEnzyme());
 describe('Component: Message', () => {
-    it('should render the Message component', () => {
+    it('should render the left Message component', () => {
         const wrapper = shallow(
-          <Message username="User 1" content="Great chatroom!" username="dros"/>
+          <Message username="User 1" content="Great chatroom!" currentUser="dros"/>
         );
         expect(wrapper.find('p').text().trim()).toEqual('Great chatroom!');
+        chai.expect(wrapper.find('div.messageLeft')).to.be.present();
+    });
+
+    it('should render the right Message component', () => {
+        const anotherWrapper = shallow(
+          <Message username="dros" content="Great chatroom again!" currentUser="dros"/>
+        );
+        expect(anotherWrapper.find('p').text().trim()).toEqual('Great chatroom again!');
+        chai.expect(anotherWrapper.find('p')).to.have.className('messageRight');
     });
 });
